6323522d6156b67b9fb275fa8c445654f2bbf5fe
anf692 <fassane348@gmail.com>
2026-05-13 20:48:06 +0000

    configurations de la vue  pour l'authentification

--- sta/views.py
+++ sta/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .models import Entry, Category
 
-# Create your views here.
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('dashboard')
+
+    def form_valid(self, form):
+        # On sauvegarde l'utilisateur
+        user = form.save()
+        # On le connecte automatiquement après inscription
+        login(self.request, user)
+        return redirect(self.success_url)
+    
+    
